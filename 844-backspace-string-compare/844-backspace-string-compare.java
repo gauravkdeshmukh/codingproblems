b94741be2c60f6560f9660b6f10a6850f143bd9c
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2022-07-20 19:47:24 -0700

    Time: 1 ms (93.96%), Space: 42.2 MB (48.06%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,25 +1,54 @@
-import java.util.*;
-
-
-
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        String stS = String.valueOf(generateStackFromString(s));
-        String stT = String.valueOf(generateStackFromString(t));
-        return stS.equals(stT);
-    }
-    
-    public Stack generateStackFromString(String s){
-        Stack<Character> st = new Stack<Character>();
-        char[] chars = s.toCharArray();
-        for(int i=0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '#' && !st.empty()){
-                st.pop();
-            }else if(c != '#'){
-               st.add(c); 
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
             }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
         }
-        return st;
+        return true;
     }
 }
+
+// simple stackbased solution
+// import java.util.*;
+// class Solution {
+//     public boolean backspaceCompare(String s, String t) {
+//         String stS = String.valueOf(generateStackFromString(s));
+//         String stT = String.valueOf(generateStackFromString(t));
+//         return stS.equals(stT);
+//     }
+    
+//     public Stack generateStackFromString(String s){
+//         Stack<Character> st = new Stack<Character>();
+//         char[] chars = s.toCharArray();
+//         for(int i=0; i < s.length(); i++){
+//             char c = s.charAt(i);
+//             if(c == '#' && !st.empty()){
+//                 st.pop();
+//             }else if(c != '#'){
+//                st.add(c); 
+//             }
+//         }
+//         return st;
+//     }
+// }
+
+// optimized logical 2 pointer solution
+
