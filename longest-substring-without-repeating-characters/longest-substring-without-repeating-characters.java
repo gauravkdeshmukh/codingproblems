ec41312b71de0c61bea2219193a94786a0aaadbd
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2022-07-22 18:05:51 -0700

    Time: 12 ms, Memory: 44.8 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,19 +1,36 @@
+// O(n^2) Brute force solution
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s.length() <= 1) return s.length();
+//         int longest = 0;
+//         for(int i=0; i < s.length(); i++){
+//             HashSet<Character> seenChars = new HashSet<Character>();
+//             for(int j=i; j < s.length(); j++){
+//                 if(!seenChars.contains(s.charAt(j))){
+//                     seenChars.add(s.charAt(j));
+//                     longest = Math.max(longest, seenChars.size());
+//                 }else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return longest;
+        
+//     }
+// }
+
+// O(n) Optimized solution using Sliding window technique
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         if(s.length() <= 1) return s.length();
-        int longest = 0;
-        for(int i=0; i < s.length(); i++){
-            HashSet<Character> seenChars = new HashSet<Character>();
-            for(int j=i; j < s.length(); j++){
-                if(!seenChars.contains(s.charAt(j))){
-                    seenChars.add(s.charAt(j));
-                    longest = Math.max(longest, seenChars.size());
-                }else{
-                    break;
-                }
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        int left=0; int longest = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(map.get(s.charAt(right)), left);
             }
+            longest = Math.max(longest, right - left + 1);
+            map.put(s.charAt(right), right + 1);
         }
         return longest;
-        
     }
 }
