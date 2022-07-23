f2a6dbd2166dfd134dd575d033975f6e21267ea8
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2022-07-22 18:09:01 -0700

    Time: 4 ms, Memory: 43.7 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -19,18 +19,45 @@
 // }
 
 // O(n) Optimized solution using Sliding window technique
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s.length() <= 1) return s.length();
+//         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+//         int left=0; int longest = 0;
+//         for (int right = 0; right < s.length(); right++) {
+//             if (map.containsKey(s.charAt(right))) {
+//                 left = Math.max(map.get(s.charAt(right)), left);
+//             }
+//             longest = Math.max(longest, right - left + 1);
+//             map.put(s.charAt(right), right + 1);
+//         }
+//         return longest;
+//     }
+// }
+
+// O(n) solution with sliding window 2 pointer with cache 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() <= 1) return s.length();
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-        int left=0; int longest = 0;
-        for (int right = 0; right < s.length(); right++) {
-            if (map.containsKey(s.charAt(right))) {
-                left = Math.max(map.get(s.charAt(right)), left);
+        
+        int n = s.length();
+        
+        int start = 0;
+        int end = 0;
+        int str[] = new int[128];
+        int max = 0;
+        
+        while(end < n) {
+            char curr = s.charAt(end);
+            str[curr]++;
+            
+            while( str[curr] > 1) {
+                str[s.charAt(start)]--;
+                start++;
             }
-            longest = Math.max(longest, right - left + 1);
-            map.put(s.charAt(right), right + 1);
+            max = Math.max(max, end - start + 1);
+            end++;
         }
-        return longest;
+        
+        return max;
     }
 }
