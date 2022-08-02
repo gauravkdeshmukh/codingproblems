bf13c417c1814dd47e92de761ec99e6fdd8887fd
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2022-08-01 20:34:20 -0700

    Time: 1 ms (100.00%), Space: 42.3 MB (94.87%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,21 +1,28 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
-        int singleNum = -1;
-        for(int num: nums){
-            if(!map.containsKey(num)){
-                map.put(num, 1);
-            }else{
-               map.put(num, map.get(num)+1); 
-            }
-        }
+//         HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+//         int singleNum = -1;
+//         for(int num: nums){
+//             if(!map.containsKey(num)){
+//                 map.put(num, 1);
+//             }else{
+//                map.put(num, map.get(num)+1); 
+//             }
+//         }
         
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            if(entry.getValue() == 1){
-                singleNum = entry.getKey();
-            }
-        }
-        return singleNum;
+//         for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+//             if(entry.getValue() == 1){
+//                 singleNum = entry.getKey();
+//             }
+//         }
+//         return singleNum;
         
+//     }
+    int a = 0;
+    for (int i : nums) {
+      //we can XOR all bits together to find the unique number  
+      a ^= i;
     }
+    return a;
+  }
 }
