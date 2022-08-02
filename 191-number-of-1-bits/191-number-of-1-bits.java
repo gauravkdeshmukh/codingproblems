3fc8c8d1c4531078a6d9f79191dbb828220911ff
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2022-08-01 19:28:58 -0700

    Time: 1 ms (87.00%), Space: 41.9 MB (11.46%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -1,11 +1,25 @@
 public class Solution {
     // you need to treat n as an unsigned value
+    // simple counting the number of 1s in binary form
+    // public int hammingWeight(int n) {
+    //    String binaryStr = Integer.toBinaryString(n);
+    //    int count = 0;
+    //     for(int i=0; i<binaryStr.length(); i++){
+    //      if(binaryStr.charAt(i) == '1')  count += 1; 
+    //    } 
+    //   return count;
+    // }
+    
     public int hammingWeight(int n) {
-       String binaryStr = Integer.toBinaryString(n);
-       int count = 0;
-        for(int i=0; i<binaryStr.length(); i++){
-         if(binaryStr.charAt(i) == '1')  count += 1; 
-       } 
-      return count;
+        int count = 0;
+        while(n != 0){
+            count += 1;
+            //doing a bit-wise AND of nn and n - 1 flips the least-significant 1-bit in n to 0
+            //the least significant 1-bit in n always corresponds to a 0-bit in n - 1
+            // anding the two numbers n and n - 1 always flips the least significant 1-bit in n to 0, and keeps all other bits the same.            
+            n &= (n - 1);
+        }
+        return count;
     }
+    
 }
