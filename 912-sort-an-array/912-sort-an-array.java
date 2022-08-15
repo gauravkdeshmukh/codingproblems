ce909f8775175ab2fb60c4995c4aff34366e0d84
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2022-08-14 23:13:47 -0700

    Time: 823 ms (12.62%), Space: 80.9 MB (5.24%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,23 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        int offset = 50000;
+        int freq[] = new int[100005];
+        for(int element : nums){
+            freq[element+offset]++;
+            System.out.print(""+ freq[element+offset]);
+        }
+        
+        System.out.println("");
+        
+        int index=0;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+             nums[index++] = i-offset;
+                System.out.print("" + (i-offset));        
+                freq[i]--;
+            }
+        }
+        
         return nums;
     }
 }
