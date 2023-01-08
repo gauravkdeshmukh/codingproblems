8cf31af133a3d86f775f0862f1e49156b5e64cac
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2023-01-07 20:21:29 -0800

    Time: 10 ms (98.88%), Space: 40.2 MB (94.70%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,20 +1,30 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int n = points.length;
-        if (n == 1) {
-            return 1;
-        }
-        int result = 2;
-        for (int i = 0; i < n; i++) {
-            Map<Double, Integer> cnt = new HashMap<>();
-            for (int j = 0; j < n; j++) {
-                if (j != i) {
-                    cnt.merge(Math.atan2(points[j][1] - points[i][1],
-                    	points[j][0] - points[i][0]), 1, Integer::sum);
+
+        int result = 1;
+
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+
+                int count = 2;
+
+                for (int k = j + 1; k < points.length; k++) {
+                    int x1 = points[i][0];
+                    int y1 = points[i][1];
+                    int x2 = points[j][0];
+                    int y2 = points[j][1];
+                    int x3 = points[k][0];
+                    int y3 = points[k][1];
+
+                    if ((y2 - y1) * (x3 - x2) == (y3 - y2) * (x2 - x1)) {
+                        count++;
+                    }
                 }
+
+                result = Math.max(result, count);
             }
-            result = Math.max(result, Collections.max(cnt.values()) + 1);
         }
+
         return result;
     }
 }
