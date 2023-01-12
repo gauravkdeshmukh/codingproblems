3c412352c78c2913029650935b579e8b0bdc5b91
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2023-01-11 18:26:08 -0800

    Time: 1 ms (63.72%), Space: 41.9 MB (99.55%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -4,6 +4,7 @@ class Solution {
         int dSum = 0;
         for(int i=0;i<n;i++){
             for(int j=0; j<n;j++){
+                // 0-based index so other diagonal would be at (n-1-j)
                 if(i == j || (n-1-j) == i){
                     dSum += mat[i][j];
                 }
