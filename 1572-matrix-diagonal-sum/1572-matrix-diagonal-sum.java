47ecc9c41a2c0c50768d721232e172e286a9cc0d
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2023-01-11 18:29:04 -0800

    Time: 0 ms (100.00%), Space: 42.7 MB (61.11%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int n = mat.length;
-        int dSum = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0; j<n;j++){
-                // 0-based index so other diagonal would be at (n-1-j)
-                if(i == j || (n-1-j) == i){
-                    dSum += mat[i][j];
-                }
+        int sum = 0;
+        for(int i=0 ; i<mat.length ; i++){
+            sum += mat[i][i];
+
+            // check for the comming cell for primary and secondary diagonal
+            if(i != mat.length-i-1){
+                sum += mat[i][mat.length-i-1];
             }
         }
-        return dSum;
+
+        return sum;
     }
 }
