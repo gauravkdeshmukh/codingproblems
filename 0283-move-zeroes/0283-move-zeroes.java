006769681e1370cd938d02c5a027e20697945d67
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2023-01-11 18:17:49 -0800

    Time: 2 ms (71.91%), Space: 43.7 MB (78.11%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    private int[] swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }
+    
+    public void moveZeroes(int[] nums) {
+        int start = 0;
+        int nonZeroAt = 0;
+        while(start < nums.length){ //O(n)
+            if(nums[start] != 0){
+                nums = swap(nums, start, nonZeroAt);
+                nonZeroAt += 1;
+            }
+            start += 1;
+        }
+    }
+}
