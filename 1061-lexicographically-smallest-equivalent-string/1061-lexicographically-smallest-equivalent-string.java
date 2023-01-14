e1c5d653922d39dc2c45edaea7023f143e6777c9
Gaurav Deshmukh, MS, MBA <gauravkdeshmukh@users.noreply.github.com>
2023-01-14 15:27:03 -0800

    Time: 1 ms (100.00%), Space: 40.5 MB (96.02%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -1,49 +1,49 @@
 class Solution {
-    int representative[] = new int[26];
+    private static final int R = 26;
 
-    // Returns the root representative of the component.
-    int find(int x) {
-        if (representative[x] == x) {
-            return x;
+    private static class UnionFind {
+        private int[] parent;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            for (int i=0; i < n; i++) parent[i] = i;
         }
 
-        return representative[x] = find(representative[x]);
+        public int find(int x) {
+            int y = parent[x];
+            if (y != x) {
+                y = find(y);
+                parent[x] = y;
+            }
+
+            return y;
+        }
+
+        public void connect(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+            if (px < py) {
+                parent[py] = px;
+            } else if (px > py) {
+                parent[px] = py;
+            }
+        }
     }
 
-    // Perform union if x and y aren't in the same component.
-    void performUnion(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x == y) {
-            return;
-        }
-
-        // Make the smaller character representative.
-        if (x < y) {
-            representative[y] = x;
-        } else {
-            representative[x] = y;
-        }
-    }
-
-    public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        // Make each character representative of itself.
-        for (int i = 0; i < 26; i++) {
-            representative[i] = i;
-        }
-
-        // Perform union merge for all the edges.
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {        
+        UnionFind uf = new UnionFind(26);
         for (int i = 0; i < s1.length(); i++) {
-            performUnion(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            uf.connect(c1 - 'a', c2 - 'a');
         }
 
-        String ans = "";
-        // Create the answer string with final mappings.
-        for (char c : baseStr.toCharArray()) {
-            ans += (char)(find(c - 'a') + 'a');
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < baseStr.length(); i++) {
+            char c = baseStr.charAt(i);
+            char c2 = (char) ('a' + uf.find(c - 'a'));
+            builder.append(c2);
         }
-
-        return ans;
+        return builder.toString();
     }
 }
